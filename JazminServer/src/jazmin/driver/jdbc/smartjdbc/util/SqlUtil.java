537e85b92e40c63af1ef838f15f2c16b8ef69b23
sky <sky@cornerstone365.cn>
2022-04-14 16:59:37 +0800

    add

--- JazminServer/src/jazmin/driver/jdbc/smartjdbc/util/SqlUtil.java
+++ JazminServer/src/jazmin/driver/jdbc/smartjdbc/util/SqlUtil.java
@@ -9,22 +9,38 @@ import jazmin.driver.jdbc.smartjdbc.enums.DatabaseType;
  */
 public class SqlUtil {
 
-	/*
-	 * 
-	 */
 	public static String identifier(DatabaseType type) {
 		if(type.equals(DatabaseType.MYSQL)) {
 			return "`";
 		}
-		if(type.equals(DatabaseType.POSTGRESQL)) {
+		if(type.equals(DatabaseType.POSTGRESQL)||
+				type.equals(DatabaseType.KINGBASE)||
+				type.equals(DatabaseType.NDS)) {
 			return "\"";
-		}
-		if(type.equals(DatabaseType.KINGBASE)) {
-			return "\"";
-		}
-		if(type.equals(DatabaseType.NDS)) {
-			return "";
 		}
 		return "";
+	}
+	
+	public static boolean isBasePostgresql(DatabaseType type) {
+		if(isPostgresql(type)||isKingbasesql(type)||isNds(type)) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static boolean isPostgresql(DatabaseType type) {
+		return type.equals(DatabaseType.POSTGRESQL);
+	}
+	
+	public static boolean isKingbasesql(DatabaseType type) {
+		return type.equals(DatabaseType.KINGBASE);
+	}
+	
+	public static boolean isNds(DatabaseType type) {
+		return type.equals(DatabaseType.NDS);
+	}
+	
+	public static boolean isMysql(DatabaseType type) {
+		return type.equals(DatabaseType.MYSQL);
 	}
 }
