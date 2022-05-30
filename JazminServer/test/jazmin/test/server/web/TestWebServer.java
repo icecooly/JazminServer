6a853ce2c51f5bb333dd74121d4093351be8f770
sky <sky@cornerstone365.cn>
2022-05-30 19:42:25 +0800

    fastjson

--- JazminServer/test/jazmin/test/server/web/TestWebServer.java
+++ JazminServer/test/jazmin/test/server/web/TestWebServer.java
@@ -19,26 +19,17 @@ public class TestWebServer {
 	 */
 	public static void main(String[] args) throws Exception{
 		//
-		LoggerFactory.setLevel("ALL");
+		LoggerFactory.setLevel("INFO");
 		LoggerFactory.setFile("/tmp/" + TestWebServer.class.getSimpleName() + ".log", true);
 		//
 		//
 		Jazmin.setServerName("boss"+System.getProperty("user.name"));
-		JazminRpcDriver rpcDriver=new JazminRpcDriver();
-        rpcDriver.addRemoteServer("TableItAccountSystem", "app", "127.0.0.1", 9996);
-        rpcDriver.addRemoteServer("TableItBizSystem_uat_0", "app", "127.0.0.1", 9997);
-        Jazmin.addDriver(rpcDriver);
-		//
-//		FileServerDriver fileServerDriver=new FileServerDriver();
-//		fileServerDriver.setHomeDir("local_file_storage");
-//		fileServerDriver.addServer("YcytFileSystem","wx.yicyt.com",8604,1);
-//		Jazmin.addDriver(fileServerDriver);
 		//
 		WebServer ws=new WebServer();
 		ws.setEnableJettyLogger(true);
-		ws.addResource("/","/Users/skydu/eclipse-workspace11/TableItWebSystem/release");
+		ws.addWar("/","/Users/skydu/eclipse-workspace/Jetty/release/DemoWebSystem-1.0.war");
 		Jazmin.addServer(ws);
-		ws.setPort(7003);
+		ws.setPort(8888);
 		Jazmin.start();
 		
 	}
