46eca89933b5f675b0c9f7c750f7ec3fc014b9f0
height-peak <707914178@qq.com>
2023-03-10 16:10:40 +0800

    classUtil...

---
+++ JazminServer/src/jazmin/util/ClassUtil.java
@@ -0,0 +1,34 @@
+package jazmin.util;
+
+import java.lang.reflect.Type;
+
+/**
+ * @author liutao
+ * @date 2023/3/10 15:03
+ */
+public class ClassUtil {
+
+
+    /**
+     * 获得给定类的第一个泛型参数
+     *
+     * @param clazz 被检查的类，必须是已经确定泛型类型的类
+     * @return {@link Class}
+     */
+    public static Class<?> getTypeArgument(Class<?> clazz) {
+        return getTypeArgument(clazz, 0);
+    }
+
+    /**
+     * 获得给定类的泛型参数
+     *
+     * @param clazz 被检查的类，必须是已经确定泛型类型的类
+     * @param index 泛型类型的索引号，即第几个泛型类型
+     * @return {@link Class}
+     */
+    public static Class<?> getTypeArgument(Class<?> clazz, int index) {
+        final Type argumentType = TypeUtil.getTypeArgument(clazz, index);
+        return TypeUtil.getClass(argumentType);
+    }
+    
+}
